a1e67924dc2f656f5b0209ca5b83d0ec69aab413
Reini Urban <rurban@x-ray.at>
2014-08-25 19:34:53 +0000

    Import of RURBAN/Bundle-CPANReporter2-0.10 from CPAN.
    gitpan-cpan-distribution: Bundle-CPANReporter2
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         RURBAN/Bundle-CPANReporter2-0.10.tar.gz
    gitpan-cpan-author:       RURBAN
    gitpan-cpan-maturity:     released

--- CPANReporter2.pm
+++ CPANReporter2.pm
@@ -1,6 +1,6 @@
 package Bundle::CPANReporter2;
 
-$VERSION = '0.09';
+$VERSION = '0.10';
 
 1;
 
@@ -35,6 +35,8 @@ Only once:
 =head1 CONTENTS
 
 Params::Util		1.07
+
+HTML::Parser            3.71
 
 Data::UUID		1.219
 
@@ -96,7 +98,12 @@ Test::Reporter::Transport::Metabase 1.999008
 I have a hierarchy of @INC so I mostly have to install only into some
 lower versioned perl. Just XS modules need a manual update then. For
 this complicated dependency chain I used this bundle, esp. for
-Data::UUID.
+Data::UUID, but over time more and more dependencies gone missing.
+Latest additions to the club: Params::Util HTML::Parser
+
+=head1 DEVELOPMENT
+
+L<https://github.com/rurban/Bundle-CPANReporter2>
 
 =head1 AUTHOR
 
